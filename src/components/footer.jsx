e2d0c107472db3b0b7b3521ca4362dfb1e626506
pr4j3sh <prajesh.eleven118@gmail.com>
2025-02-24 16:49:56 +0530

    updated author

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ export default function Footer() {
   return (
     <footer>
       <p>
-        <a href="https://pr4j3sh.vercel.app/" target="_blank">
+        <a href="https://pr4j3sh.github.io/pr4j3sh/" target="_blank">
           pr4j3sh
         </a>
       </p>
